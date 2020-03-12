9b8471433f8a40a91f10f2dc033db825008db9b0
Wegig_Ciang <wegig.arok@ti.ukdw.ac.id>
2020-03-13 01:50:26 +0700

    Android Minggu 5

---
+++ app/src/main/java/com/example/helloworld/DatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.example.helloworld;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table user(email text primary key, username text, password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists user");
+    }
+    //insert
+    public boolean insert(String email, String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email",email);
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long ins = db.insert("user", null, contentValues);
+        if(ins==-1) return false;
+        else return true;
+    }
+    //Cek apakah email sudah ada atau belom
+    public Boolean checkmail(String email){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM user WHERE email=?", new String[]{email});
+        if(cursor.getCount()>0) return false;
+        else return true;
+    }
+
+    //cek email sama pass pas login
+    public Boolean emailPass(String email, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM user WHERE email=? and password=?", new String[]{email,password});
+        if(cursor.getCount()>0) return true;
+        else return false;
+    }
+}
--- app/src/main/java/com/example/helloworld/FragmentProfile.java
+++ app/src/main/java/com/example/helloworld/FragmentProfile.java
@@ -1,6 +1,9 @@
 package com.example.helloworld;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.helloworld.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -22,12 +28,36 @@ public class FragmentProfile extends Fragment {
         // Required empty public constructor
     }
 
-
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_fragment_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //kenalin button dari xml
+        Button logOut = (Button) view.findViewById(R.id.logOutMain);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preference = getActivity().getSharedPreferences("masuk", Context.MODE_PRIVATE);
+                //SharedPreferences preference = getSharedPreferences("masuk", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preference.edit();
+                editor.putString("ingat", "false");
+                editor.apply();
+                Intent gotoprofile = new Intent(getActivity(), MainActivity.class);
+                startActivity(gotoprofile);
+            }
+        });
+
+    }
+
+
 
 }
